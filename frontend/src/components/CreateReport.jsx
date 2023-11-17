f684a57feff6f2e637dd1c6da269dcc57086192f
ltidy <ltidy@uwo.ca>
2023-11-17 12:28:56 -0500

    Update CreateReport.jsx
    added the dropdown with searching capabilitities- version 1, may chance later to use material UI

--- frontend/src/components/CreateReport.jsx
+++ frontend/src/components/CreateReport.jsx
@@ -17,11 +17,61 @@ function CreateReport() {
                 <Form.Group className="mb-3">
                     <Form.Label>Building</Form.Label>
                     <Form.Select style={{"color": "grey"}}>
-                        <option>Select the building where the issue is located</option>
-                        {/* TODO: add options for every building */}
-                        <option value="1">Amit Chakma Engineering Building</option>
-                        <option value="2">Natural Sciences Centre</option>
-                        <option value="3">Social Sciences Centre</option>
+                        <option>Select the building where the issue is located</option>         
+                        <option value="1">3M</option>
+                        <option value="2">Advanced Faculty For Avian Research</option>
+                        <option value="3">Alumni Hall</option>
+                        <option value="4">Amit Chakma Engineering</option>
+                        <option value="5"> Arts and Humanity Building</option>
+                        <option value="6">Biological and Geological Sciences Building</option>
+                        <option value="7"> Chemistry Building</option>
+                        <option value="8">Claudette Mackay-Lassonde Pavilion</option>
+                        <option value="9">Clinical Skills Building (Dr. Don Rix)</option>
+                        <option value="10">Dental Sciences Building</option>
+                        <option value="11">Elborn College</option>
+                        <option value="12">Faculty of Education Building </option>
+                        <option value="13">FIMS and Nursing Building</option>
+                        <option value="14">Health Science Addition</option>
+                        <option value="15"> Health Science Building</option>
+                        <option value="16">International and Graduate </option>
+                        <option value="17">Ivey Building</option>
+                        <option value="18">Kresge Building </option>
+                        <option value="19">Law Building </option>
+                        <option value="20">Lawson Building</option>
+                        <option value="21">Material Science Addition </option>
+                        <option value="22">Medical Sciences Building</option>
+                        <option value="23">Middlesex College</option>
+                        <option value="24">Molecular Biology Laboratory</option>
+                        <option value="25"> Music Building</option>
+                        <option value="26">Natural Science Centre</option>
+                        <option value="27">North Campus Building</option>
+                        <option value="28">Ontario Hall Residence</option>
+                        <option value="29">Physics and Astronomy Building</option>
+                        <option value="30">Robarts Research Institute</option>
+                        <option value="31">Siebens-Drake Research Institute</option>
+                        <option value="32">Social Science Centre</option>
+                        <option value="33">Somerville House</option>
+                        <option value="34">Spencer Engineering Building</option>
+                        <option value="35">Staging Building</option>
+                        <option value="36"> Stevenson Hall</option>
+                        <option value="37">Support Services Building</option>
+                        <option value="38">Talbot College</option>
+                        <option value="39">Taylor Library</option>
+                        <option value="40"> Western Alumni Stadium</option>
+                        <option value="41">Thames Hall</option>
+                        <option value="42">Thompson Engineering Building</option>
+                        <option value="43">Thompson Recreation and Athletic Centre</option>
+                        <option value="44">University College</option>
+                        <option value="45">University Community Centre</option>
+                        <option value="46"> Visual Arts Centre</option>
+                        <option value="47">D.B. Weldon Library</option>
+                        <option value="48">Westren Centre for Public Health and Family Medicine</option>
+                        <option value="49"> Western Interdisciplinary Research Building</option>
+                        <option value="50">Western Science Centre</option>
+                        <option value="51">Western Recreation Centre</option>
+                        <option value="52">Western Student Services Building</option>
+                        <option value="53">Westminister Hall</option>
+
                     </Form.Select>    
                 </Form.Group>
 
