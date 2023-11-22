64156384a777ab0fc7db031ddedf254ca3c6f6e3
pmoeini1 <pmoeini@uwo.ca>
2023-11-22 11:43:00 -0500

    admin login progress

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 import HeaderLogo from "./HeaderLogo";
 
 function AdminLogin() {
+    const [admin, setAdmin] = useState(null);
+    const [password, setPassword] = useState(null);
     return (
         <div>
             <HeaderLogo />
@@ -12,14 +14,24 @@ function AdminLogin() {
             <Form>
                 <Form.Group className="mb-3" >
                     <Form.Label>Admin Username</Form.Label>
-                    <Form.Control placeholder="Enter username" />
+                    <Form.Control placeholder="Enter username" onChange={(e) => {setAdmin(e.target.value)}}/>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" />
+                    <Form.Control type="password" placeholder="Enter password" onChange={(e) => {setPassword(e.target.value)}} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" onClick={() => {
+                    if (admin && password) {
+                        const json = {
+                            admin, 
+                            password
+                        }
+                        // TODO: send to backend for verification
+                    } else {
+                        window.alert("fill out both username and password")
+                    }
+                }}>
                     Submit
                 </Button>
             </Form>
