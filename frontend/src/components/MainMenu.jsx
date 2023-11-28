6f0b16aa310a88bef19f4be1d769367d05d68a28
Oreoluwa Collins <ocollin4@uwo.ca>
2023-11-28 09:44:32 -0500

    Pushing css file

--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import HeaderLogo from "./HeaderLogo";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "../styles.css";
 
 function MainMenu() {
     return (
@@ -10,14 +11,14 @@ function MainMenu() {
                 <div class="column">
             <HeaderLogo />
             </div>
-            <div class ="column" style ={{ "float": "right"}}>
+            <div class ="column">
             <ButtonGroup vertical>
-            <Button variant="info"><Link to="/createReport" style= {{"textDecoration" :"none", "color":"black"}}>Create Report</Link></Button>
-            <Button variant="info"><Link to="/adminLogin" style= {{"textDecoration" :"none", "color":"black"}}>Admin Login</Link></Button>
-            <Button variant="info"><Link to="/predictor" style= {{"textDecoration" :"none", "color":"black"}}>Predictor Model</Link></Button>
-            <Button variant="info"><Link to="/trends" style= {{"textDecoration" :"none", "color":"black"}}>View Trends</Link></Button>
-            <Button variant="info"><Link to="/map" style= {{"textDecoration" :"none", "color":"black"}}>View Map</Link></Button>
-            <Button variant="info"><Link to="/reports" style= {{"textDecoration" :"none", "color":"black"}}>View Reports</Link></Button>
+            <Button variant="info" ><Link to="/createReport">Create Report</Link></Button>
+            <Button variant="info"><Link to="/adminLogin">Admin Login</Link></Button>
+            <Button variant="info"><Link to="/predictor">Predictor Model</Link></Button>
+            <Button variant="info"><Link to="/trends">View Trends</Link></Button>
+            <Button variant="info"><Link to="/map" >View Map</Link></Button>
+            <Button variant="info"><Link to="/reports">View Reports</Link></Button>
         </ButtonGroup>
         </div>
         </div>
