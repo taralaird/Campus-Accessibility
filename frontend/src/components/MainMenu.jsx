2a9b2e6fd6dc7835d4bf896303c743d126ed0400
Oreoluwa Collins <ocollin4@uwo.ca>
2023-11-30 18:50:57 -0500

    Added content

--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -3,24 +3,58 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import HeaderLogo from "./HeaderLogo";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import MapImage from "../images/Campus_Map_2-1.png";
 import "../styles.css";
+
 
 function MainMenu() {
     return (
-            <div class="row">
-                <div class="column">
+        <div className="main-page">
+            <div className="nav-menu">
+                <div className="logo-container">
             <HeaderLogo />
+                </div>
+                <div className ="button-container">
+                <ButtonGroup vertical>
+                <Button variant="link" className="button"><Link to="/createReport" className="button-link">Create Report</Link></Button>
+                <Button variant="link" className="button"><Link to="/adminLogin" className="button-link">Admin Login</Link></Button>
+                <Button variant="link" className="button"><Link to="/predictor" className="button-link">Predictor Model</Link></Button>
+                <Button variant="link" className="button"><Link to="/trends" className="button-link">View Trends</Link></Button>
+                <Button variant="link" className="button"><Link to="/map" className="button-link">View Map</Link></Button>
+                <Button variant="link" className="button"><Link to="/reports" className="button-link">View Reports</Link></Button>
+                </ButtonGroup>
+                </div>
             </div>
-            <div class ="column">
-            <ButtonGroup vertical>
-            <Button variant="info" ><Link to="/createReport">Create Report</Link></Button>
-            <Button variant="info"><Link to="/adminLogin">Admin Login</Link></Button>
-            <Button variant="info"><Link to="/predictor">Predictor Model</Link></Button>
-            <Button variant="info"><Link to="/trends">View Trends</Link></Button>
-            <Button variant="info"><Link to="/map" >View Map</Link></Button>
-            <Button variant="info"><Link to="/reports">View Reports</Link></Button>
-        </ButtonGroup>
-        </div>
+
+            <div className="content">
+                <div className="map-sec">
+                <img src={MapImage} alt="Map" />
+                <Button variant="info" className="expand-button">Expand Map</Button>
+                </div>
+
+                <div className="newest-sec">
+
+                    <div className="newest1">
+                        <h2>Newest Trends</h2>
+                        <div>
+                        <div className="image-placeholder">
+                                {/* TrendsSection component goes here when available */}
+                                <p>Image Placeholder</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="newest2">
+                        <h2>Newest Reports</h2>
+                        <div>
+                        <div className="image-placeholder">
+                                {/* TrendsSection component goes here when available */}
+                                <p>Image Placeholder</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
