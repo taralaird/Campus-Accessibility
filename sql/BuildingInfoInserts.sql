bac275464972ecde612ddfdbabd198fc6241e690
ohoward2 <71092087+ohoward2@users.noreply.github.com>
2024-03-12 20:20:24 -0400

    Insert statements for Building Information

---
+++ sql/BuildingInfoInserts.sql
@@ -0,0 +1,154 @@
+USE campus_accessibility;
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("3M", 3, 6,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Alumni Hall", 3, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Amit Chakma Engineering", 4, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Arts and Humanity Building", 4, 3,  "y", "n", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Biological and Geological Sciences Building", 5, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Chemistry Building", 6, 2,  "y", "n", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Claudette Mackay-Lassonde Pavilion", 5, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Clinical Skills Building", 4, 5,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Dental Sciences Building", 9, 5,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Elborn College", 2, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Faculty of Education Building", 2, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("FIMS and Nursing Building", 6, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Health Science Addition", 7, 7,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Health Science Building", 4, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("International and Graduate Building", 4, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Ivey Building", 5, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Kresge Building", 5, 5,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Law Building", 2, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Lawson Building", 4, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Material Science Addition", 5, 6,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Medical Sciences Building", 7, 5,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Middlesex College", 5, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Molecular Biology Labratory", 5, 7,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Music Building", 7, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Natural Science Centre", 10, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("North Campus Building", 5, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Physics and Astronomy Building", 4, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Robarts Research Insitute", 8, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Siebens-Drake Research Institute", 3, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Social Science Centre", 9, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Somerville House", 4, 6,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Spencer Engineering Building", 5, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Staging Building", 2, 1,  "y", "n", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Stevenson Hall", 4, 4,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Support Services Building", 6, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Talbot College", 6, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Taylor Library", 5, 5,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Alumni Stadium", 2, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Thames Hall", 3, 6,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Thompson Engineering Building", 6, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Thompson Recreation and Athletic Centre", 4, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("University College", 4, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("University Community Centre", 5, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Visual Arts Centre", 3, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("D.B. Weldon Library", 7, 3,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Centre for Public Health and Family Medicine", 4, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Interdisciplinary Research Building", 8, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Science Centre", 4, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Recreation Centre", 4, 1,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Western Student Services Building", 5, 2,  "y", "y", "y");
+
+INSERT INTO `Building_Information` (`BuildingName`, `NumberOfFloors`, `NumberofElevators`, `BarrierFreeWashrooms`, `GenderNeutralWashrooms`, `AutomaticButtonEntry`) VALUES
+("Westminister Hall", 4, 2,  "y", "n", "y");
